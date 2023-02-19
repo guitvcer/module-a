92253204b1e0196b499a42aad96a17c468136b24
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-02-19 12:09:15 +0600

    set username as unique

--- authorization/models.py
+++ authorization/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=60, verbose_name='Username')
+    username = models.CharField(max_length=60, unique=True, verbose_name='Username')
     password = models.CharField(max_length=2**16, verbose_name='Password')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
     is_blocked = models.BooleanField(default=False, verbose_name='Is Blocked?')
