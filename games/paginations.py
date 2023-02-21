916feb1d694b59e5c7e94d2306a0a1149405a8c2
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-02-21 20:16:26 +0600

    fix game pagination page size

--- games/paginations.py
+++ games/paginations.py
@@ -3,8 +3,7 @@ from rest_framework.response import Response
 
 
 class GamePagination(PageNumberPagination):
-    # page_size = 10
-    page_size = 2
+    page_size = 10
     page_size_query_param = 'size'
 
     def get_paginated_response(self, data: dict) -> Response:
