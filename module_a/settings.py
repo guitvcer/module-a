4d0ded77107ae079b8438fc93ca8f808f45f73e5
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-02-22 22:05:53 +0600

    install djangorestframework-camel-case

--- module_a/settings.py
+++ module_a/settings.py
@@ -102,6 +102,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'authorization.authentication.Authentication',
     ],
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+    ),
     'EXCEPTION_HANDLER': 'core.utils.custom_exception_handler',
     'ORDERING_PARAM': 'sort_by',
 }
