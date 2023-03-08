b51617b2528fb6fa18f6b047ee74e5c40b83e4ee
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-08 18:09:49 +0600

    return unrealeases games if user views his details

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import RetrieveAPIView
 
 from authorization.models import User
+from authorization.permissions import IsAuthenticated
 from .serializers import UserSerializer
 
 
@@ -8,3 +9,9 @@ class UserView(RetrieveAPIView):
     serializer_class = UserSerializer
     lookup_field = 'username'
     queryset = User.objects.filter(is_active=True)
+    permission_classes = (IsAuthenticated, )
+
+    def get_serializer_context(self) -> dict:
+        return {
+            'user': self.request.user,
+        }
