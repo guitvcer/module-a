621a8ebabdbd59e6d69b6cdf4b61586f0b5e295d
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-09 18:21:53 +0600

    add score model to admin

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Game, GameVersion
+from .models import Game, GameVersion, Score
 
 admin.site.register(Game)
 admin.site.register(GameVersion)
+admin.site.register(Score)
