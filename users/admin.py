3b1de9dc6c5d53e9e6b04e025bd7725a3f61c2ea
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2023-03-09 20:31:15 +0600

    add deactivate users action

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import QuerySet
 
 from .models import User
 
-admin.site.register(User)
+@admin.action(description='Deactivate selected users')
+def deactivate_users(modeladmin: UserAdmin, request: WSGIRequest, queryset: QuerySet) -> None:
+    queryset.update(is_active=False)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    actions = (deactivate_users, )
